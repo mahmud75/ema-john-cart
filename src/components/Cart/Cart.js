fbf68c0a3ff0afef536ed45026dc3a0093e9d7d6
Sultan <slsultan76@gmail.com>
2023-01-22 00:12:12 +0600

    test

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,7 @@ const Cart = ({cartData}) => {
         <div>
             <h3>This order summary</h3>
             <h4>Selected Items: {cartData.length}</h4>
+            <h3>hello world</h3>
         </div>
     );
 };
